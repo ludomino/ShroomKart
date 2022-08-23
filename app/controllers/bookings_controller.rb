fe0b42abfce021cb2a974df43ffa83e896f54a30
ludovic guadagnin <guadagnin.ludovic@gmail.com>
2022-08-23 15:42:08 +0200

    page profil test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-   before_action :set_booking, only: :destroy
+  before_action :set_booking, only: :destroy
   before_action :set_list, only: [:new, :create]
 
   def show
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     @karts = Kart.all
   end
+
+  def profile
+    @user = User.find(params[:id])
+  end
 end
